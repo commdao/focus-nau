3cbbd4204d7e2899dadc9bce93a4b8c66753f642
Jon Dao <62568113+commdao@users.noreply.github.com>
2023-08-24 16:09:04 -0400

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,131 @@
+const display = document.getElementById('remaining-time-display');
+
+const startButton = document.getElementById('startButton');
+const resetButton = document.getElementById('resetButton');
+const addTimeButton = document.getElementById('addTimeButton');
+const subtractTimeButton = document.getElementById('subtractTimeButton');
+
+let isRunning = false;
+let timer = 0;
+let intervalId;
+let totalTimeInSeconds = 0;
+
+startButton.addEventListener('click', () => {
+    if (!isRunning) {
+        startTimer();
+        isRunning = true;
+        startButton.textContent = 'Pause';
+        updateBackgroundColor(taskTypeSelect.value);
+    } else {
+        pauseTimer();
+        isRunning = false;
+        startButton.textContent = 'Resume';
+    }
+});
+
+resetButton.addEventListener('click', () => {
+    resetTimer();
+    updateBackgroundColor('');
+    clearInputFields();
+    bannerText.style.display = 'none';
+});
+
+addTimeButton.addEventListener('click', () => {
+    addTime(5);
+});
+
+subtractTimeButton.addEventListener('click', () => {
+    subtractTime(5);
+})
+
+function clearInputFields() {
+    document.getElementById('hoursInput').value = '';
+    document.getElementById('minutesInput').value = '';
+}
+
+function startTimer() {
+    if (timer === 0) {
+    const hours = parseInt(document.getElementById('hoursInput').value) || 0;
+    const minutes = parseInt(document.getElementById('minutesInput').value) || 0;
+
+    totalTimeInSeconds = hours * 3600 + minutes * 60;
+    timer = totalTimeInSeconds;
+    }
+
+    intervalId = setInterval(() => {
+            if (timer <= 0) {
+                clearInterval(intervalId);
+                display.innerHTML = "<span class='times-up'>Time Is Up!</span>";
+                shakeScreen();
+                isRunning = false;
+                startButton.textContent = 'Start';
+
+                const bannerText = document.getElementById('banner-text');
+                if (totalTimeInSeconds <= 1800) {
+                    playAudioCue('winning');
+                } else {
+                    playAudioCue('major_triumph');
+                }
+
+                return;
+            }
+
+            timer--;
+            updateDisplay();
+    }, 1000);
+}
+
+function playAudioCue(cueName) {
+    const audioPath = `/media/${cueName}.m4a`;
+    const audioElement = new Audio(audioPath);
+    audioElement.play();
+}
+
+function pauseTimer() {
+    clearInterval(intervalId);
+}
+
+function updateDisplay() {
+        display.innerHTML = formatTime(timer);
+}
+
+function formatTime(timeInSeconds) {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+}
+
+function padZero(number) {
+    return number.toString().padStart(2, '0');
+}
+
+function shakeScreen() {
+        display.classList.add('shake');
+        setTimeout(() => {
+                display.classList.remove('shake');
+        }, 500);
+}
+
+function addTime(minutesToAdd) {
+    timer += minutesToAdd * 60;
+    updateDisplay();
+}
+
+function subtractTime(minutesToSubtract) {
+    timer -= minutesToSubtract * 60;
+    if (timer < 0) {
+        timer = 0;
+    }
+    updateDisplay();
+}
+
+function resetTimer() {
+    clearInterval(intervalId);
+    isRunning = false;
+    startButton.textContent = 'Start';
+    timer = 0;
+    totalTimeInSeconds = 0;
+    updateDisplay();
+}
---
+++ icons.js
@@ -0,0 +1,117 @@
+const taskTypeSelect = document.getElementById('task-type');
+const businessTasks = document.getElementById('businessTasks');
+const pleasureTasks = document.getElementById('pleasureTasks');
+
+toggleTaskTypeIcons('business');
+
+taskTypeSelect.addEventListener('change', () => {
+    const selectedValue = taskTypeSelect.value;
+
+    toggleTaskTypeIcons(selectedValue);
+    updateBackgroundColor(selectedValue);
+});
+
+function toggleTaskTypeIcons(taskType) {
+
+    if (taskType === 'business') {
+        businessTasks.style.display = 'flex';
+        pleasureTasks.style.display = 'none';
+    } else if (taskType === 'pleasure') {
+        pleasureTasks.style.display = 'flex';
+        businessTasks.style.display = 'none';
+    } else {
+        businessTasks.style.display = 'none';
+        pleasureTasks.style.display = 'none';
+    }
+}
+
+
+function updateBackgroundColor(taskType) {
+    document.body.classList.remove('business-mode', 'pleasure-mode');
+    if (taskType === 'business') {
+        document.body.classList.add('business-mode');
+    } else if (taskType === 'pleasure') {
+        document.body.classList.add('pleasure-mode');
+    }
+}
+
+const taskNames = {
+    business: ['BUSINESS', 'STUDY', 'EXERCISE', 'WRITING'],
+    pleasure: ['GAMING', 'PRACTICE', 'OTHER']
+};
+const bannerText = document.getElementById('banner-text');
+const taskIcons = document.querySelectorAll('.tasks i');
+
+const taskAudioPaths = {
+    BUSINESS: 'media/business.m4a', 
+    STUDY: 'media/study.m4a',
+    EXERCISE: 'media/exercise.m4a',
+    WRITING: 'media/writing.m4a',
+    GAMING: 'media/gaming.m4a',
+    PRACTICE: 'media/practice.m4a',
+    OTHER: 'media/other.m4a'
+};
+
+const taskAudioElements = {};
+
+Object.keys(taskAudioPaths).forEach(taskName => {
+    const audioElement = new Audio(taskAudioPaths[taskName]);
+    audioElement.addEventListener('play', () => {
+        console.log(`${taskName} audio started playing`);
+    });
+    audioElement.addEventListener('ended', () => {
+        console.log(`${taskName} audio finished playing`);
+    });
+    audioElement.volume = 1;
+    taskAudioElements[taskName] = audioElement;
+});
+
+taskIcons.forEach(icon => {
+    icon.style.display = 'flex';
+    icon.addEventListener('mouseover', () => {
+        const taskName = icon.getAttribute('data-task');
+        displayBannerText(taskName, true);
+    });
+    icon.addEventListener('mouseout', () => {
+        displayBannerText('', false);
+    });
+    icon.addEventListener('click', () => {
+        const taskName = icon.getAttribute('data-task');
+        playAudioClip(icon.getAttribute('data-task'));
+        enlargeIcon(icon);
+        hideOtherIcons(icon.parentNode, taskName);
+        bannerText.style.display = 'none';
+    });
+});
+
+function displayBannerText(taskName, show) {
+    bannerText.textContent = show ? taskName : '';
+    bannerText.style.display = show ? 'block' : 'none';
+}
+
+function playAudioClip(taskName) {
+    if (taskAudioElements.hasOwnProperty(taskName)) {
+        const audioElement = taskAudioElements[taskName];
+        audioElement.play();
+    }
+}
+
+function enlargeIcon(icon) {
+    icon.style.transform = 'scale(1.5)';
+    icon.style.transition = 'font-size 0.5s';
+}
+
+function hideOtherIcons(iconContainer, excludedTask) {
+    const icons = iconContainer.querySelectorAll('.tasks i');
+    icons.forEach(icon => {
+        if (icon.getAttribute('data-task') !== excludedTask) {
+            icon.style.display = 'none';
+        }
+    });
+}
+
+const resetIcons = document.getElementById('reset-icons-button');
+resetIcons.addEventListener('click', () => {
+        location.reload();
+})
+
